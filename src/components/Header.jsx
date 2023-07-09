d3f4a27c085e6648df5e61cb4c6e25ef0e42d25e
javiergp23 <javgon23@hotmail.com>
2023-07-09 14:49:07 -0300

    changes09

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,32 @@ const Header = () => {
                         <p className='text-smallTextColor text-[14px] font-[500px]'>Personal</p>
                     </div>
                 </div>
+                {/* ======== logo end =========*/}
+                {/* ======== menu start ========*/}
+                <div className='menu'>
+                    <ul className='flex items-center gap-10'>
+                        <li>
+                            <a className='text-smallTextColor font-[600]' href="#about">About</a>
+                        </li>
+                        <li>
+                            <a className='text-smallTextColor font-[600]' href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a className='text-smallTextColor font-[600]' href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a className='text-smallTextColor font-[600]' href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                {/* ======== menu end =========*/}
+                {/* ======== menu right =========*/}
+                <div className="flex items-center gap-4">
+                    <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColorpy-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-700'>
+                        <i class="ri-send-plane-line"></i> Let's Talk
+                    </button>
+                </div>
+                {/* ======== menu end =========*/}
             </div>
         </div>
     </header>
