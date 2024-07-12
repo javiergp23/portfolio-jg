718581b48058f962814509f266a1581dbe6ab03c
Javier Gonzalez <javgon2306@gmail.com>
2024-07-12 11:08:52 -0300

    changes services 01

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -8,7 +8,7 @@ import appsImg from '../../assets/images/apps.png';
 const Services = () => {
   return (
     <section id='services'> 
-        <div className='container lg:pt-5'>
+        <div className='container lg:pt-5'> 
             <div className='text-center'>
                 <h2 className='text-headingColor font-[800] text-[2.4rem] mb-5'>
                     Conocimientos y Tecnologias
